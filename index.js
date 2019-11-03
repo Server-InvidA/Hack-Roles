f99706604c70ee4d54107848265af7ea151471a6
Server-InvidA <57058961+Server-InvidA@users.noreply.github.com>
2019-11-03 10:51:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -114,10 +114,11 @@ bot.on('message', message => {
 	}*/
 });
 
-/*bot.on('messageReactionAdd', (reaction, user) => {
+bot.on('messageReactionAdd', (reaction, user) => {
 	if(reaction.emoji.name === ":arrow_right:") {
-		let role = user.guild.roles.find('name', 'test')
+		let role = reaction.guild.roles.find('name', 'Observateur')
+		reaction.member.addRole(role)
 	}
-});*/
+});
 
 bot.login(process.env.TOKEN);

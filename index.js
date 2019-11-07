f1571385ace46cdac459dbf8623ed5ab7a72648e
Server-InvidA <57058961+Server-InvidA@users.noreply.github.com>
2019-11-07 17:02:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -105,7 +105,7 @@ bot.on('message', message => {
 					.addField("$helpa : Affiche la liste des commandes du serveur discord (commandes admin inclues)", "Utilisation : $help")
 					.addField("$jet : Faire un jet de dés de 100", "Utilisation : $jet")
 					.addField("$jet 10 ou 5 ... : Faire un jet de dés de la valeur que vous voulez", "Utilisation : $jet <nombre maximum>")
-					.addField("$new : Création de votre personnage", "Utilisation: $new <name> <surname> <description> {<nombre physique> <nombre social> <nombre mental> <nombre dextérité>}<-- Total: 200");
+					.addField("$new : Création de votre personnage", "Utilisation: $new <name> <surname> <description> {<nombre physique> <nombre social> <nombre mental> <nombre dextérité>}<-- Total: 200")
 					.addField("$jet : Lancement du jeu", "Utilisation : $go");
 				message.channel.send(gradesEmbed);
 				message.delete();
